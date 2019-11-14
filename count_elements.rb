bb92b2028d6fa9fd95af0a2adb53abbe14e27c65
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-11-14 13:39:55 -0700

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  results = {}
+  array.each do |animal|
+    if results[animal] != nil
+      results[animal] += 1
+    else
+      results[animal] = 1
+    end
+  end
+  return results
 end
  
